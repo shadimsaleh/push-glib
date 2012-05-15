3201c24655cabe8f756def94c89babf376b3167a
Christian Hergert <chris@dronelabs.com>
2012-05-14 18:57:51 -0700

    [c2dm-client] Add debug macros to important methods.

--- push-glib/push-c2dm-client.c
+++ push-glib/push-c2dm-client.c
@@ -19,6 +19,7 @@
 #include <glib/gi18n.h>
 
 #include "push-c2dm-client.h"
+#include "push-debug.h"
 
 G_DEFINE_TYPE(PushC2dmClient, push_c2dm_client, SOUP_TYPE_SESSION_ASYNC)
 
@@ -47,18 +48,28 @@ void
 push_c2dm_client_set_auth_token (PushC2dmClient *client,
                                  const gchar    *auth_token)
 {
+   ENTRY;
    g_return_if_fail(PUSH_IS_C2DM_CLIENT(client));
    g_free(client->priv->auth_token);
    client->priv->auth_token = g_strdup(auth_token);
    g_object_notify_by_pspec(G_OBJECT(client), gParamSpecs[PROP_AUTH_TOKEN]);
+   EXIT;
 }
 
 static void
 push_c2dm_client_finalize (GObject *object)
 {
-   PushC2dmClientPrivate *priv = PUSH_C2DM_CLIENT(object)->priv;
+   PushC2dmClientPrivate *priv;
+
+   ENTRY;
+
+   priv = PUSH_C2DM_CLIENT(object)->priv;
+
    g_free(priv->auth_token);
+
    G_OBJECT_CLASS(push_c2dm_client_parent_class)->finalize(object);
+
+   EXIT;
 }
 
 static void
@@ -100,17 +111,32 @@ push_c2dm_client_class_init (PushC2dmClientClass *klass)
 {
    GObjectClass *object_class;
 
+   ENTRY;
+
    object_class = G_OBJECT_CLASS(klass);
    object_class->finalize = push_c2dm_client_finalize;
    object_class->get_property = push_c2dm_client_get_property;
    object_class->set_property = push_c2dm_client_set_property;
    g_type_class_add_private(object_class, sizeof(PushC2dmClientPrivate));
+
+   gParamSpecs[PROP_AUTH_TOKEN] =
+      g_param_spec_string("auth-token",
+                          _("Auth Token"),
+                          _("The authentication token to use with C2DM."),
+                          NULL,
+                          G_PARAM_READWRITE);
+   g_object_class_install_property(object_class, PROP_AUTH_TOKEN,
+                                   gParamSpecs[PROP_AUTH_TOKEN]);
+
+   EXIT;
 }
 
 static void
 push_c2dm_client_init (PushC2dmClient *client)
 {
+   ENTRY;
    client->priv = G_TYPE_INSTANCE_GET_PRIVATE(client,
                                               PUSH_TYPE_C2DM_CLIENT,
                                               PushC2dmClientPrivate);
+   EXIT;
 }
