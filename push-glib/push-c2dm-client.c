13c91592342dc9a87f182a9b7fc77fc043753a53
Christian Hergert <chris@dronelabs.com>
2012-05-14 18:17:25 -0700

    [push-glib] Initial commit.

---
+++ push-glib/push-c2dm-client.c
@@ -0,0 +1,116 @@
+/* push-c2dm-client.c
+ *
+ * Copyright (C) 2012 Christian Hergert <chris@dronelabs.com>
+ *
+ * This file is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <glib/gi18n.h>
+
+#include "push-c2dm-client.h"
+
+G_DEFINE_TYPE(PushC2dmClient, push_c2dm_client, SOUP_TYPE_SESSION_ASYNC)
+
+struct _PushC2dmClientPrivate
+{
+   gchar *auth_token;
+};
+
+enum
+{
+   PROP_0,
+   PROP_AUTH_TOKEN,
+   LAST_PROP
+};
+
+static GParamSpec *gParamSpecs[LAST_PROP];
+
+const gchar *
+push_c2dm_client_get_auth_token (PushC2dmClient *client)
+{
+   g_return_val_if_fail(PUSH_IS_C2DM_CLIENT(client), NULL);
+   return client->priv->auth_token;
+}
+
+void
+push_c2dm_client_set_auth_token (PushC2dmClient *client,
+                                 const gchar    *auth_token)
+{
+   g_return_if_fail(PUSH_IS_C2DM_CLIENT(client));
+   g_free(client->priv->auth_token);
+   client->priv->auth_token = g_strdup(auth_token);
+   g_object_notify_by_pspec(G_OBJECT(client), gParamSpecs[PROP_AUTH_TOKEN]);
+}
+
+static void
+push_c2dm_client_finalize (GObject *object)
+{
+   PushC2dmClientPrivate *priv = PUSH_C2DM_CLIENT(object)->priv;
+   g_free(priv->auth_token);
+   G_OBJECT_CLASS(push_c2dm_client_parent_class)->finalize(object);
+}
+
+static void
+push_c2dm_client_get_property (GObject    *object,
+                               guint       prop_id,
+                               GValue     *value,
+                               GParamSpec *pspec)
+{
+   PushC2dmClient *client = PUSH_C2DM_CLIENT(object);
+
+   switch (prop_id) {
+   case PROP_AUTH_TOKEN:
+      g_value_set_string(value, push_c2dm_client_get_auth_token(client));
+      break;
+   default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
+   }
+}
+
+static void
+push_c2dm_client_set_property (GObject      *object,
+                               guint         prop_id,
+                               const GValue *value,
+                               GParamSpec   *pspec)
+{
+   PushC2dmClient *client = PUSH_C2DM_CLIENT(object);
+
+   switch (prop_id) {
+   case PROP_AUTH_TOKEN:
+      push_c2dm_client_set_auth_token(client, g_value_get_string(value));
+      break;
+   default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
+   }
+}
+
+static void
+push_c2dm_client_class_init (PushC2dmClientClass *klass)
+{
+   GObjectClass *object_class;
+
+   object_class = G_OBJECT_CLASS(klass);
+   object_class->finalize = push_c2dm_client_finalize;
+   object_class->get_property = push_c2dm_client_get_property;
+   object_class->set_property = push_c2dm_client_set_property;
+   g_type_class_add_private(object_class, sizeof(PushC2dmClientPrivate));
+}
+
+static void
+push_c2dm_client_init (PushC2dmClient *client)
+{
+   client->priv = G_TYPE_INSTANCE_GET_PRIVATE(client,
+                                              PUSH_TYPE_C2DM_CLIENT,
+                                              PushC2dmClientPrivate);
+}
---
+++ push-glib/push-c2dm-client.h
@@ -0,0 +1,67 @@
+/* push-c2dm-client.h
+ *
+ * Copyright (C) 2012 Christian Hergert <chris@dronelabs.com>
+ *
+ * This file is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef PUSH_C2DM_CLIENT_H
+#define PUSH_C2DM_CLIENT_H
+
+#include <glib-object.h>
+#include <libsoup/soup.h>
+
+#include "push-c2dm-identity.h"
+#include "push-c2dm-message.h"
+
+G_BEGIN_DECLS
+
+#define PUSH_TYPE_C2DM_CLIENT            (push_c2dm_client_get_type())
+#define PUSH_C2DM_CLIENT(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), PUSH_TYPE_C2DM_CLIENT, PushC2dmClient))
+#define PUSH_C2DM_CLIENT_CONST(obj)      (G_TYPE_CHECK_INSTANCE_CAST ((obj), PUSH_TYPE_C2DM_CLIENT, PushC2dmClient const))
+#define PUSH_C2DM_CLIENT_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass),  PUSH_TYPE_C2DM_CLIENT, PushC2dmClientClass))
+#define PUSH_IS_C2DM_CLIENT(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), PUSH_TYPE_C2DM_CLIENT))
+#define PUSH_IS_C2DM_CLIENT_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass),  PUSH_TYPE_C2DM_CLIENT))
+#define PUSH_C2DM_CLIENT_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj),  PUSH_TYPE_C2DM_CLIENT, PushC2dmClientClass))
+
+typedef struct _PushC2dmClient        PushC2dmClient;
+typedef struct _PushC2dmClientClass   PushC2dmClientClass;
+typedef struct _PushC2dmClientPrivate PushC2dmClientPrivate;
+
+struct _PushC2dmClient
+{
+   SoupSessionAsync parent;
+
+   /*< private >*/
+   PushC2dmClientPrivate *priv;
+};
+
+struct _PushC2dmClientClass
+{
+   SoupSessionAsyncClass parent_class;
+};
+
+GType    push_c2dm_client_get_type       (void) G_GNUC_CONST;
+void     push_c2dm_client_deliver_async  (PushC2dmClient       *client,
+                                          PushC2dmIdentity     *identity,
+                                          PushC2dmMessage      *message,
+                                          GAsyncReadyCallback   callback,
+                                          gpointer              user_data);
+gboolean push_c2dm_client_deliver_finish (PushC2dmClient       *client,
+                                          GAsyncResult         *result,
+                                          GError              **error);
+
+G_END_DECLS
+
+#endif /* PUSH_C2DM_CLIENT_H */
